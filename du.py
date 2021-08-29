4e14502104f25f72f65f024bdca2c0be0d1f3bc6
siroen <mturu1976@gmail.com>
2021-08-29 16:14:30 +0900

    def pwdcmdを追加

--- du.py
+++ du.py
@@ -1,7 +1,11 @@
 from sh import du
 from sh import sort
-
+from sh import pwd
 
 def ducmd():
     comannds = sort(du("-d 1", "-h"), "-rn")
+    return comannds
+
+def pwdcmd():
+    comannds = pwd()
     return comannds
